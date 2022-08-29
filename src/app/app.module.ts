c0d8df2e225d317e4b12c12efaecc1410a54e8e3
Javier Colmenar <fjcolmenar@serbatic.es>
2022-08-29 14:06:14 +0200

    Firebase init and ng add @angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'carmaintenance-14a24',
+    appId: '1:1006230016815:web:9b9ad928e531c8759387da',
+    storageBucket: 'carmaintenance-14a24.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDxn3i4SaV94IOSzn62vn4u2H1zu9P6dZU',
+    authDomain: 'carmaintenance-14a24.firebaseapp.com',
+    messagingSenderId: '1006230016815',
+  },
   production: false
 };
 
